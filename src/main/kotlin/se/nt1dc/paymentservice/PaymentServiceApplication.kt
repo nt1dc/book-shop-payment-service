6283f8311c684ffc2485d9ef5d51091853fed878
nt1dc <nt1dcare@gmail.com>
2023-03-17 14:27:25 +0300

    init

---
+++ src/main/kotlin/se/nt1dc/paymentservice/PaymentServiceApplication.kt
@@ -0,0 +1,11 @@
+package se.nt1dc.paymentservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentServiceApplication>(*args)
+}
